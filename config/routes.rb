29c7506fc6348415e7ae5197e702f139975041ed
tiffanynuugen <tiffanynuugen@gmail.com>
2019-04-12 17:39:03 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  resources :songs, only: [:index, :show, :new, :create, :edit, :update, :destroy]
-  resources :artists, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  resources :artists, :songs 
   post 'songs/upload', to: 'songs#upload'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
