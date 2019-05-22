6e14457d2ed270748ac998ccbd387d3dfe82864a
github username <github email address>
2019-05-22 10:08:27 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,5 @@
 class SongsController < ApplicationController
+require File.expand_path('../../config/environment', __FILE__)
 
   def index
     @songs = Song.all
@@ -51,4 +52,3 @@ class SongsController < ApplicationController
     params.require(:song).permit(:title, :artist_name)
   end
 end
-
