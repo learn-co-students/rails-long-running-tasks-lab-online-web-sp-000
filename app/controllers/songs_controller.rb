e80b60c95d374065cffed29e19710a79771d1fa9
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-07-04 20:28:36 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -50,7 +50,7 @@ class SongsController < ApplicationController
     CSV.foreach(params[:file].path, headers: true) do |song|
       new_song = Song.new
       new_song.title = song[0]
-      new_song.artist = Artist.find_or_create_by(song[1])
+      new_song.artist = Artist.find_or_create_by(name:song[1])
       new_song.save
     end
 
